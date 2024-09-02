7652b2cc53082b60a90f8a3343c12023c600ec4c
LokTedd4000 <julietanferreyra@yahoo.com.ar>
2024-09-01 23:31:14 -0300

    Add content portfolio, codiv-19

--- js/index.js
+++ js/index.js
@@ -37,21 +37,3 @@ navLinks.forEach(link => {
 });
 
 
-/*PagesUp*/
-
-var btn = $('#button');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
-
-
